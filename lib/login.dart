d106152dd83424678f2659799a4dc3a0e3880ec3
Harshit jain <harshit2020cs198@abesit.edu.in>
2023-06-04 23:56:29 +0530

    login added

--- lib/login.dart
+++ lib/login.dart
@@ -1,14 +1,29 @@
 import 'package:consciousleap/Activity_page1.dart';
 import 'package:consciousleap/Activity_page21.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_borders/gradient_borders.dart';
 
-class login extends StatelessWidget {
+class login extends StatefulWidget {
+  @override
+  State<login> createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  final email = TextEditingController();
+  final phone = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  String Email="";
+  String Phone="";
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(),
-      body: Row(
+      body:Form(
+        key: _formKey,
+        child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
@@ -39,6 +54,7 @@ class login extends StatelessWidget {
                         ),
                         borderRadius: BorderRadius.circular(12)),
                     child: TextField(
+                      controller: email,
                       decoration: InputDecoration(
                         border: InputBorder.none,
 
@@ -69,6 +85,7 @@ class login extends StatelessWidget {
                         ),
                         borderRadius: BorderRadius.circular(12)),
                     child: TextField(
+                      controller: phone,
                       decoration: InputDecoration(
                         border: InputBorder.none,
 
@@ -90,9 +107,14 @@ class login extends StatelessWidget {
                 child:ElevatedButton(
                   child:Text("Login" ,style:TextStyle(fontSize:12,color: Colors.white,fontFamily:'Comforta')),
                   onPressed: (){
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) =>Activity_page21()
-                        ));
+                    if(_formKey.currentState!.validate()){
+                      setState(() {
+                        Email=email.text.trim();
+                        Phone=phone.text.trim();
+                        
+                      });
+                      signinUser();
+                    }
                   },
                   style: ElevatedButton.styleFrom(
                     primary: Colors.blueAccent,
@@ -107,6 +129,39 @@ class login extends StatelessWidget {
           ),
         ],
       ),
+      )
+
     );
   }
+  void signinUser() async {
+    // ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(""+Phone+Email)));
+
+    try{
+      UserCredential userCredential=await FirebaseAuth.instance.signInWithEmailAndPassword(email:Email, password:Phone);
+      print(userCredential);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Login succesfully")));
+
+      // String? id = FirebaseAuth.instance.currentUser?.uid;
+      // UserModel userModel=UserModel(id:id,firstName: firstname, lastName: lastname, email: email, password: password);
+      // db.collection("doctor").doc(password).set(userModel.toJson());
+    } on FirebaseAuthException catch (e){
+      if(e.code=='wrong-password'){
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Wrong password.")));
+
+      }else if(e.code=='invalid-email'){
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Invalid email.")));
+
+      }
+      else if(e.code=='user-not-found'){
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("" + e.code.toString())));
+
+      }
+      else{
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Network Issue.")));
+
+      }
+    }
+  }
+
 }
+
