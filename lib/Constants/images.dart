ed8ae9ae4866a282a9e872b21be2fe2604623340
Harshit jain <harshit2020cs198@abesit.edu.in>
2023-07-30 13:23:39 +0530

    Product Details Updated

---
+++ lib/Constants/images.dart
@@ -0,0 +1,5 @@
+const catImg1='assets/images/product1.jpg';
+const catImg2='assets/images/product1.jpg';
+const catImg3='assets/images/product1.jpg';
+const catImg4='assets/images/product1.jpg';
+const catImg5='assets/images/product1.jpg';
---
+++ lib/conscious_store/ProductDetails.dart
@@ -0,0 +1,155 @@
+import 'package:consciousleap/Constants/images.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class ProductDetails extends StatefulWidget {
+  const ProductDetails({Key? key}) : super(key: key);
+
+  @override
+  State<ProductDetails> createState() => _ProductDetailsState();
+}
+
+class _ProductDetailsState extends State<ProductDetails> {
+  String _selectedSize = 'Option 1'; // Initial selected item
+
+  List<String> _dropdownSize = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
+
+  String _selectedFabric='Fabric1';
+  List<String> _dropdownFabric=['Fabric1','Fabric2','Fabric3'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.white,
+          constraints: BoxConstraints.expand(),
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                Image.asset(catImg1,height: 400,fit: BoxFit.fill,).box.white.margin(EdgeInsets.all(8)).border(color: Colors.grey,).make(),
+
+                Container(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      "T-Shirt White".text.size(28).black.semiBold.make(),
+                      8.heightBox,
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15.0,vertical: 5),
+                        child: Row(
+
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            "Share :".text.size(18).make(),
+
+                            Icon(Icons.share,color: Colors.black,),
+
+                          ],
+                        ),
+                      ),
+                      Divider(color: Colors.grey,thickness: 1,height: 1,),
+                      10.heightBox,
+                      "Size :".text.gray400.size(18).align(TextAlign.start).make(),
+                     5.heightBox,
+                  DropdownButton<String>(
+                    value: _selectedSize,
+                    underline: Container(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedSize = newValue!;
+                      });
+                    },
+                    items: _dropdownSize.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList()).box.border(color: Colors.black12,width: 1.5).roundedSM.height(40).padding(EdgeInsets.symmetric(horizontal: 3)).make(),
+                      15.heightBox,
+                      "Fabric :".text.gray400.size(18).align(TextAlign.start).make(),
+                      5.heightBox,
+                      DropdownButton<String>(
+                          value: _selectedFabric,
+                          underline: Container(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              _selectedFabric = newValue!;
+                            });
+                          },
+                          items: _dropdownFabric.map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList()).box.roundedSM.border(color: Colors.black12,width: 1.5).height(40).padding(EdgeInsets.symmetric(horizontal: 3)).make(),
+                      15.heightBox,
+                      "Fabric :".text.gray400.size(18).align(TextAlign.start).make(),
+                      3.heightBox,
+                      "AED100.00".text.lineThrough.size(28).blue300.make(),
+                      15.heightBox,
+                      "Fabric :".text.gray400.size(18).align(TextAlign.start).make(),
+                      3.heightBox,
+                      "AED95.00".text.size(28).blue600.make(),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.blueAccent),
+                        onPressed: (){},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.shopping_cart,color: Colors.white,),
+                            10.widthBox,
+                            "Add to cart".text.center.white.semiBold.make()
+                          ],
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                        onPressed: (){},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            "Buy it now".text.white.semiBold.make()
+                          ],
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.blueAccent),
+                        onPressed: (){},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            "Add to Whishlist".text.white.semiBold.make()
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ).box.white.margin(EdgeInsets.symmetric(vertical: 2,horizontal: 8)).padding(EdgeInsets.all(8)).border(color: Colors.grey).make(),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      "Description".text.align(TextAlign.left).gray400.semiBold.size(21).make(),
+                      5.heightBox,
+                      Divider(height: 1,thickness: 1,color: Colors.grey,),
+                      10.heightBox,
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+                      .text.justify.gray800.make()
+                    ],
+                  ),
+                ).box.white.margin(EdgeInsets.symmetric(vertical: 2,horizontal: 8)).padding(EdgeInsets.all(8)).border(color: Colors.grey).make()
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
